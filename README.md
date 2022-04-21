0d7cf5729effaac908916b3db5eca4f72581eba8
ersen <70ozturkersen@gmail.com>
2022-04-21 15:47:02 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,78 @@
+
+
+# Project-008 : Movie App 
+
+## Description
+
+Project aims to create a Movie App.
+
+
+## Project Skeleton
+
+```
+008 - Movie App (folder)
+|
+|----readme.md         # Given to the students (Definition of the project)
+SOLUTION
+├── public
+│     └── index.html
+├── src
+│    ├── auth
+│    │     └── firebase.js
+│    ├── components
+│    │     ├── MovieCard.js
+│    │     └── Navbar.js
+│    ├── context
+│    │     └── AuthContext.js
+│    ├── pages
+│    │     ├── Login.js
+│    │     ├── Register.js
+│    │     ├── Main.js
+│    │     └── MovieDetail.js
+│    ├── router
+│    │     └── Router.js
+│    ├── App.js
+│    ├── App.css
+│    ├── index.js
+│    └── index.css
+├── package.json
+├── .env
+└── yarn.lock
+```
+
+## Expected Outcome
+
+![Project 008 Snapshot](movie-app.gif)
+
+## Objective
+
+Build a Movie App using ReactJS.
+
+### At the end of the project, following topics are to be covered;
+
+- HTML
+
+- CSS
+
+- JS
+
+- ReactJS
+
+### At the end of the project, students will be able to;
+
+- improve coding skills within HTML & CSS & JS & ReactJS.
+
+- use git commands (push, pull, commit, add etc.) and Github as Version Control System.
+
+## Steps to Solution
+
+- Step 1 : Create React App using `npx create-react-app movie-app`
+
+- Step 2 : Signup `https://firebase.google.com/` and create new app in firebase.
+
+- Step 3 : Use `https://firebase.google.com/docs/auth/web/start` and create `Authentication` operations.
+
+- Step 4 : Signup `https://www.themoviedb.org/documentation/api` and get API key for getting data from `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`, for searching movies `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=` and for movie details `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`.
+
+
+**<p align="center">&#9786; Happy Coding &#9997;</p>**
---
+++ src/auth/firebase.js
@@ -0,0 +1,126 @@
+import { initializeApp } from "firebase/app";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import {
+  toastErrorNotify,
+  toastSuccessNotify,
+  toastWarnNotify,
+} from "../helpers/ToastNotify";
+
+//* https://firebase.google.com/docs/auth/web/start
+//* https://console.firebase.google.com/ => project settings
+//! firebase console settings bölümünden firebaseconfig ayarlarını al
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBinUBbqxMSlrfRKx1JkEvtcTbUPkHzkms",
+  authDomain: "react-firebase-33dca.firebaseapp.com",
+  projectId: "react-firebase-33dca",
+  storageBucket: "react-firebase-33dca.appspot.com",
+  messagingSenderId: "566200334051",
+  appId: "1:566200334051:web:09b5903b3523a18f5af7d6",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+
+export const createUser = async (email, password, displayName, navigate) => {
+  try {
+    //? yeni bir kullanıcı oluşturmak için kullanılan firebase metodu
+    let userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    //? kullanıcı profilini güncellemek için kullanılan firebase metodu
+    await updateProfile(auth.currentUser, {
+      displayName: displayName,
+    });
+    navigate("/");
+    toastSuccessNotify("Registered successfully!");
+    console.log(userCredential);
+  } catch (err) {
+    toastErrorNotify(err.message);
+    // alert(err.message);
+  }
+};
+
+//* https://console.firebase.google.com/
+//* => Authentication => sign-in-method => enable Email/password
+//! Email/password ile girişi enable yap
+export const signIn = async (email, password, navigate) => {
+  try {
+    //? mevcut kullanıcının giriş yapması için kullanılan firebase metodu
+    let userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    navigate("/");
+    toastSuccessNotify("Logged in successfully!");
+    console.log(userCredential);
+  } catch (err) {
+    toastErrorNotify(err.message);
+    // alert(err.message);
+  }
+};
+
+export const logOut = () => {
+  signOut(auth);
+  toastSuccessNotify("Logged out successfully!");
+};
+
+export const userObserver = (setCurrentUser) => {
+  //? Kullanıcının signin olup olmadığını takip eden ve kullanıcı değiştiğinde yeni kullanıcıyı response olarak dönen firebase metodu
+  onAuthStateChanged(auth, (currentUser) => {
+    if (currentUser) {
+      setCurrentUser(currentUser);
+    } else {
+      // User is signed out
+      setCurrentUser(false);
+    }
+  });
+};
+
+//* https://console.firebase.google.com/
+//* => Authentication => sign-in-method => enable Google
+//! Google ile girişi enable yap
+export const signUpProvider = (navigate) => {
+  //? Google ile giriş yapılması için kullanılan firebase metodu
+  const provider = new GoogleAuthProvider();
+  //? Açılır pencere ile giriş yapılması için kullanılan firebase metodu
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      console.log(result);
+      navigate("/");
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      console.log(error);
+    });
+};
+
+export const forgotPassword = (email) => {
+  //? Email yoluyla şifre sıfırlama için kullanılan firebase metodu
+  sendPasswordResetEmail(auth, email)
+    .then(() => {
+      // Password reset email sent!
+      toastWarnNotify("Please check your mail box!");
+      // alert("Please check your mail box!");
+    })
+    .catch((err) => {
+      toastErrorNotify(err.message);
+      // alert(err.message);
+      // ..
+    });
+};
